6b83f8efa039f9ce116ede0b8217ac1aa272dcc4
wy373226722 <a@12345679.cn>
2017-05-12 20:44:59 +0800

    学习

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  width: 100%;
+  height: 100%;
+}
+</style>
